b3f4b21ea87e75df44f41a2e4a19cabdb9714840
CodesRahul <codesrahul96@gmail.com>
2024-11-30 14:00:54 +0530

    fix social

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -81,23 +81,23 @@ const details = [
 
 const Projects = () => {
   return (
-    <section id="Projects" className="text-white bg-[#121212] relative">
+    <section id="Projects" className="text-white relative">
       <div className="flex flex-col items-center justify-center px-12 py-14 mx-32 xlx:mx-20 lgx:mx-10 mdx:mx-7 msx:mx-4 msx:px-4">
         <h1 className="text-5xl font-centra font-bold mb-5 "> Projects </h1>
         <p className="font-poppins text-center mb-6">
           “Done is better than perfect.”
           <br /> Here, You Find Some Project's Done By Me.. 😊
         </p>
-        <div className="grid grid-cols-3 gap-4 w-full  lgx:grid-cols-2 smx:grid-cols-1">
-          <ProjectCard value={details[0]}></ProjectCard>
-          <ProjectCard value={details[1]}></ProjectCard>
-          <ProjectCard value={details[2]}></ProjectCard>
-          <ProjectCard value={details[3]}></ProjectCard>
-          <ProjectCard value={details[4]}></ProjectCard>
-          <ProjectCard value={details[5]}></ProjectCard>
-          <ProjectCard value={details[6]}></ProjectCard>
-          <ProjectCard value={details[7]}></ProjectCard>
-          <ProjectCard value={details[8]}></ProjectCard>
+        <div className="w-full grid grid-cols-3 gap-4 lgx:grid-cols-2 smx:grid-cols-1">
+          <ProjectCard value={details[0]} />
+          <ProjectCard value={details[2]} />
+          <ProjectCard value={details[1]} />
+          <ProjectCard value={details[3]} />
+          <ProjectCard value={details[4]} />
+          <ProjectCard value={details[5]} />
+          <ProjectCard value={details[6]} />
+          <ProjectCard value={details[7]} />
+          <ProjectCard value={details[8]} />
         </div>
       </div>
     </section>
